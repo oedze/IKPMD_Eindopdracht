3538bfd0488d15926c34bec4b261b097e330f808
Oetze van den Broek <s1102228@student.hsleiden.nl>
2020-01-14 16:09:01 +0100

    Added test id

--- app/src/main/java/ikpmd/ikpmd/testapplication/models/StepResult.java
+++ app/src/main/java/ikpmd/ikpmd/testapplication/models/StepResult.java
@@ -5,13 +5,15 @@ public class StepResult {
     public String id;
     public String actualResult;
     public String passed;
+    public String testId;
 
     public StepResult() {}
 
-    public StepResult(String id, String actualResult, String passed) {
+    public StepResult(String id, String actualResult, String passed, String testId) {
         this.id = id;
         this.actualResult = actualResult;
         this.passed = passed;
+        this.testId = testId;
     }
 
 }
