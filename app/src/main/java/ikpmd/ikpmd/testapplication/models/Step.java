f8ea97b56c0664ef39899e0730b19fd157d3da0b
0x13f <twarmerdam14@gmail.com>
2020-01-14 14:14:46 +0100

    Firebase verbinding

--- app/src/main/java/ikpmd/ikpmd/testapplication/models/Step.java
+++ app/src/main/java/ikpmd/ikpmd/testapplication/models/Step.java
@@ -3,15 +3,17 @@ package ikpmd.ikpmd.testapplication.models;
 public class Step {
 
     public String id;
+    public int number;
     public String details;
     public String expectedResult;
 
     public Step() {}
 
-    public Step(String id, String details, String expectedResult) {
+    public Step(String id, String details, String expectedResult, int number) {
         this.id = id;
         this.details = details;
         this.expectedResult = expectedResult;
+        this.number = number;
     }
 
 }
